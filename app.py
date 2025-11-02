a5c95ddabd0db17290ad80a15f6a7de646899cc5
otumaratoju <otumaratoju@gmail.com>
2025-11-02 21:51:00 +0100

    Improve client-side error handling and response parsing

--- app.py
+++ app.py
@@ -61,6 +61,14 @@ def init_db():
     conn.commit()
     conn.close()
 
+@app.after_request
+def add_header(response):
+    """Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes."""
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 @app.route('/')
 def home():
     """Render the main page"""
